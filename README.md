4d533a82777edfb5287d23cabe6033d565895b8b
Greg <gfitzpatrick2@gmail.com>
2023-07-24 19:38:01 -0400

    styling ignore files added, basic message event listening set up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A discord bot that consumes plex web hooks and posts them on discord
+
+### Docs
+
+https://old.discordjs.dev/#/docs/discord.js/main/general/welcome
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { Client, GatewayIntentBits, Events, Message } from 'discord.js';
+
 import { getConfig } from './utils/config';
 
 const config = getConfig();
 
-console.log(config);
+const client: Client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.on(Events.ClientReady, () => {
+  console.log(`Logged in as ${client?.user?.tag}!`);
+});
+
+client.on(Events.MessageCreate, async (message: Message) => {
+  console.log('message event hit!');
+
+  if (message.author.bot) {
+    return;
+  }
+
+  message.channel.send('PONG');
+});
+
+client.login(config.discord.token);
