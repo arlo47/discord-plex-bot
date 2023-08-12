e1deca994d34ad27b6e5751267c1785c69b16dbd
Greg <gfitzpatrick2@gmail.com>
2023-08-12 09:38:40 -0400

    test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-// import { initializeBot } from './bot/initialize';
+import { initializeBot } from './bot/initialize';
 import { getConfig } from './utils/config';
 import server from './api/server';
 
 const config = getConfig();
 
-// initializeBot();
+initializeBot();
 
 server.listen(config.server.port, () => {
   console.log(`Server listening on ${config.server.port}`);
