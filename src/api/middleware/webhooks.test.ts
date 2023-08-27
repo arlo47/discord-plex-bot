b8318fe30191a2516fdec8838758fa496e1e78f4
Arlo <gfitzpatrick2@gmail.com>
2023-08-27 15:29:49 -0400

    tests for other webhook middleware

--- src/api/middleware/webhooks.test.ts
+++ src/api/middleware/webhooks.test.ts
@@ -1,9 +1,9 @@
 import { getMockReq, getMockRes } from '@jest-mock/express';
-import { validateName } from './webhooks';
+import { parseWebHook, validateName, validateWebHookType } from './webhooks';
 
 describe('Webhooks Middleware', () => {
   describe('validateName', () => {
-    it('Should Pass If Name Is Undefined', () => {
+    it('Should pass if name is undefined', () => {
       const req = getMockReq();
       const { res, next } = getMockRes();
 
@@ -12,7 +12,7 @@ describe('Webhooks Middleware', () => {
       expect(next).toHaveBeenCalled();
     });
 
-    it('Should Pass If Name Is A String', () => {
+    it('Should pass if name is a string', () => {
       const req = getMockReq({ query: { name: 'TEST' } });
       const { res, next } = getMockRes();
 
@@ -21,11 +21,62 @@ describe('Webhooks Middleware', () => {
       expect(next).toHaveBeenCalled();
     });
 
-    it('Should Return 400 If Name Is string[]', () => {
+    it('Should return 400 if name is string[]', () => {
       const req = getMockReq({ query: { name: ['name1', 'name2'] } });
       const { res, next } = getMockRes();
 
       validateName(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+    });
+  });
+
+  describe('parseWebhook', () => {
+    it('Should pass if req.body is stringified JSON', () => {
+      const req = getMockReq({ body: JSON.stringify({ hello: 'world' }) });
+      const { res, next } = getMockRes();
+
+      parseWebHook(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('Should pass if req.body.payload is stringified JSON', () => {
+      const req = getMockReq({
+        body: { payload: JSON.stringify({ hello: 'world' }) },
+      });
+      const { res, next } = getMockRes();
+
+      parseWebHook(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe('validateWebHookType', () => {
+    it('Should pass if event type is rate', () => {
+      const req = getMockReq({ body: { event: 'media.rate' } });
+      const { res, next } = getMockRes();
+
+      validateWebHookType(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('Should return 400 if event type is pause', () => {
+      const req = getMockReq({ body: { event: 'media.pause' } });
+      const { res, next } = getMockRes();
+
+      validateWebHookType(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('Should return 400 if event type is resume', () => {
+      const req = getMockReq({ body: { event: 'media.resume' } });
+      const { res, next } = getMockRes();
+
+      validateWebHookType(req, res, next);
 
       expect(res.status).toHaveBeenCalledWith(400);
     });
