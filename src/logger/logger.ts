dae349b9bb669544de5f12da0f6ca08e5ad68cc0
Arlo <gfitzpatrick2@gmail.com>
2023-11-29 14:01:37 -0500

    added redact-secrets to redact specific fields, added custom format to handle it

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,17 +1,26 @@
 import winston, { format } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 import path from 'path';
+import Redact from 'redact-secrets';
+
+const redactor = Redact('REDACTED');
 
 const { json, timestamp, prettyPrint } = format;
 
 const redactSecrets: winston.Logform.FormatWrap = format((info) => {
-  info.eventId = '[REDACTED]';
-  return info;
+  const redacted = redactor.map(info);
+
+  // redact-secrets strips Symbols from the log, which are needed.
+  // add them back to the redacted log object
+  const levelSym = Symbol.for('level');
+  redacted[levelSym] = info[levelSym];
+
+  return redacted;
 });
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: format.combine(json(), redactSecrets(), timestamp(), prettyPrint()),
+  format: format.combine(redactSecrets(), json(), timestamp(), prettyPrint()),
   transports: [
     new DailyRotateFile({
       filename: path.join(__dirname, '/logs/bot-%DATE%.log'),
